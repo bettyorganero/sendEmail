0b8eb79d1f132f34c35e215ad32a7fa0795496e1
bettyorganero <organero@miscelaneo.net>
2024-04-17 22:03:12 +0200

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# sendEmail
+# Send-email with Node.js 📨
+### Using nodemailer
+
+1. Create a config.js file in the root 
+```
+// config.js
+module.exports = {
+    emailHost: 'your_smtp_host'
+    emailUser: 'your_email@address',
+    emailPass: 'your_email_password'
+};
+```
+2. npm install
+3. Run app: npm start
+
---
+++ server.js
@@ -0,0 +1,68 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+const cors = require('cors');
+const fs = require('fs');
+const { emailUser, emailPass } = require('./config');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors()); // Enable CORS to allow requests from Angular client
+
+// Load HTML template contents
+const recipientEmailTemplate = fs.readFileSync('recipient-email.html', 'utf-8');
+const confirmationEmailTemplate = fs.readFileSync('confirmation-email.html', 'utf-8');
+
+const transporter = nodemailer.createTransport({
+  host: emailHost, // SMTP host
+  port: 465, // Use port 465 for secure SMTP
+  secure: true, // Enable SSL/TLS
+  auth: {
+    user: emailUser, // Use username from config.js
+    pass: emailPass // Use password from config.js
+  }
+});
+
+app.post('/send-email', (req, res) => {
+  const { name, lastname, company, email, subject, message } = req.body;
+
+  // Configure email for recipient
+  const recipientMailOptions = {
+      from: email,
+      to: 'organero@miscelaneo.net',
+      subject: subject,
+      html: recipientEmailTemplate.replace(/{{name}}/g, name).replace(/{{lastname}}/g, lastname).replace(/{{company}}/g, company).replace(/{{email}}/g, email).replace(/{{message}}/g, message)
+  };
+
+  // Send email to recipient
+  transporter.sendMail(recipientMailOptions, (error, info) => {
+      if (error) {
+          console.error('Error sending email:', error);
+          res.status(500).send('Error sending email');
+      } else {
+          console.log('Email sent to recipient:', info.response);
+          // Send confirmation email to sender
+          const confirmationMailOptions = {
+              from: 'organero@miscelaneo.net',
+              to: email,
+              subject: 'Confirmation: Your message has been sent successfully',
+              html: confirmationEmailTemplate.replace(/{{name}}/g, name).replace(/{{lastname}}/g, lastname).replace(/{{company}}/g, company).replace(/{{email}}/g, email).replace(/{{message}}/g, message)
+          };
+          transporter.sendMail(confirmationMailOptions, (error, info) => {
+              if (error) {
+                  console.error('Error sending confirmation email:', error);
+              } else {
+                  console.log('Confirmation email sent:', info.response);
+              }
+          });
+          res.status(200).send('Email sent successfully');
+      }
+  });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
